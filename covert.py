df0e0d8458b3008353c79d967a6088b272cf9d78
Fireflyest <fireflyest@qq.com>
2025-01-02 19:01:35 +0300

    v3微调

--- covert.py
+++ covert.py
@@ -12,10 +12,13 @@ import mobilenetv4
 # model.eval()
 
 # 加载预训练的 MobileNetV3 模型
-model = models.mobilenet_v3_large()
+model = models.mobilenet_v3_small()
 # 修改最后一层
 num_classes = 5
-model.classifier[3] = torch.nn.Linear(model.classifier[3].in_features, num_classes)
+model.classifier[3] = torch.nn.Sequential(
+    torch.nn.Linear(model.classifier[3].in_features, num_classes),
+    torch.nn.Softmax(dim=1)
+)
 model.load_state_dict(torch.load('./out/mobilenetv3_best_finetuned.pth', weights_only=True))
 model.eval()
 
@@ -23,7 +26,7 @@ model.eval()
 
 
 # Export the model to ONNX
-out_name = './out/mobilenetv4.onnx'
+out_name = './out/mobilenet.onnx'
 dummy_input = torch.randn(1, 3, 224, 224)
 torch.onnx.export(model, dummy_input, out_name, 
                   input_names=['input'], 
